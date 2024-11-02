579db988e9b58da9c41c1fcdb92097ae675ca1d0
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-02 17:30:38 +0900

    :+1: set proxy /fastapi

--- practices/frontends/typescript-svelte/vite.config.ts
+++ practices/frontends/typescript-svelte/vite.config.ts
@@ -3,5 +3,18 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  publicDir: "static",
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    proxy: {
+      "/fastapi": {
+        target: process.env.VITE_FASTAPI_PROXY_TARGET,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fastapi/, "/api"),
+      },
+    },
+  },
   plugins: [sveltekit(), purgeCss()],
 });
