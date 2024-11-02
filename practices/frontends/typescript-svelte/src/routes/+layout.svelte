657641f7b12c771fea10c5243cc49615e1d590fe
shohei.okugi <shohei.okugi@ghelia.com>
2024-11-02 16:52:46 +0900

    :+1: adjust root layout and pages

--- practices/frontends/typescript-svelte/src/routes/+layout.svelte
+++ practices/frontends/typescript-svelte/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
   import "../app.postcss";
+  import { Toast, Modal, initializeStores } from "@skeletonlabs/skeleton";
+
+  initializeStores();
 </script>
 
-<slot />
+<svelte:head>
+  <title>title</title>
+</svelte:head>
+
+<Modal />
+<Toast position="tr" rounded="rounded-lg" />
+
+<div class="flex flex-col h-screen">
+  <slot />
+</div>
